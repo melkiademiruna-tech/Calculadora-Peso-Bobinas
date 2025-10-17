c261471e3b725331d450c71369ed3af8d375958c
melkiademiruna-tech <melkiademiruna@gmail.com>
2025-10-17 14:29:52 +0200

    Update Service-worker.js

--- Service-worker.js
+++ Service-worker.js
@@ -1,6 +1,6 @@
 // 🌀 Service Worker para la Calculadora de Peso de Bobinas
-// Versión actual: v6 — recuerda aumentar el número en cada actualización
-const CACHE_NAME = 'bobinas-cache-v6';
+// Versión actual: v3.1 — recuerda aumentar el número en cada actualización
+const CACHE_NAME = 'bobinas-cache-v3.1';
 
 // Archivos que se almacenan en caché para el modo offline
 const urlsToCache = [
@@ -40,6 +40,7 @@ self.addEventListener('activate', event => {
       .then(() => self.clients.claim())
   );
 });
+
 
 
 
